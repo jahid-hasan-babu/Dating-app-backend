6f36ae809c253d79b834ae8c5562ebd2b9bffcc7
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2025-02-19 15:34:34 +0600

    user retrive

--- src/app/modules/profiles/profile.service.ts
+++ src/app/modules/profiles/profile.service.ts
@@ -26,33 +26,36 @@ const getAllProfiles = async (userId: string, req: Request) => {
   const favoritedUserIds = new Set(
     favoriteUsers.map(fav => fav.favoritedUserId),
   );
-console.log(userId);
-// Fetch profiles with additional isFavorite field
-const result = await prisma.profile.findMany({
-  where: {
-    ...searchFilters,
-    userId: {
-      not: userId,
-    },
-  },
-  select: {
-    id: true,
-    userId: true,
-    fullName: true,
-    age: true,
-    profileImage: true,
-    isVerified: true,
-    country: true,
-    flag: true,
-    city: true,
-    user: {
-      select: {
-        status: true, // Include the status field from the user model
+
+  // Fetch profiles with additional isFavorite field
+  const result = await prisma.profile.findMany({
+    where: {
+      ...searchFilters,
+
+      userId: {
+        not: userId,
+      },
+      user: {
+        NOT: [{ accountSetup: false }],
       },
     },
-  },
-});
-
+    select: {
+      id: true,
+      userId: true,
+      fullName: true,
+      age: true,
+      profileImage: true,
+      isVerified: true,
+      country: true,
+      flag: true,
+      city: true,
+      user: {
+        select: {
+          status: true,
+        },
+      },
+    },
+  });
 
   // Map through the results to add the isFavorite field
   return result.map(profile => ({
